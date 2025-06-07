2fc2c109b9b668b63a62389b57135ff185bc8aed
nssuperx <40332687+nssuperx@users.noreply.github.com>
2025-06-07 19:02:00 +0900

    MNIST + KMNIST

--- dataset/concat_mnist.py
+++ dataset/concat_mnist.py
@@ -69,3 +69,51 @@ def generate_mnist_with_fmnist(transform: Optional[Callable] = None) -> tuple[to
     test = torch.utils.data.ConcatDataset([mnist_test, fmnist_test])
 
     return train, test
+
+
+class NotNumberLabelKMNIST(torch.utils.data.Dataset):
+    def __init__(self, data_size: int = 6000, train: bool = True, transform: Optional[Callable] = None) -> None:
+        """KMNISTを数字ではないとするデータセット
+        transformはデフォルトでtoTensor()的な動作が入る
+
+        :param data_size: 使う画像数
+        :type data_size: int
+        :param train: datasets.KMNISTのtrain引数
+        :type train: bool
+        :param transform: datasets.KMNISTで設定するtransformのようなもの。デフォルトでtoTensor()的な動作が入る
+        :type transform: Optional[Callable]"""
+        super().__init__()
+        kmnist = datasets.KMNIST("../data", train=train, download=True)
+        indices = torch.randperm(len(kmnist.data))[:data_size]
+        self.data = kmnist.data[indices].unsqueeze(1)
+        self.transform = transform
+
+    def __getitem__(self, index: int) -> tuple[torch.Tensor, int]:
+        d = self.data[index].to(dtype=torch.float32) / 255.0  # toTensor的な動作
+        if self.transform is not None:
+            d = self.transform(d)
+        return d, 10  # ラベルは適当で良い。0-9以外ならなんでもいい。数字ではないを表せたらいい。
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+
+def generate_mnist_with_kmnist(transform: Optional[Callable] = None) -> tuple[torch.utils.data.Dataset, torch.utils.data.Dataset]:
+    """MNIST + KMNISTのデータセットを返す
+
+    :param transform: datasets.MNISTで設定するtransformのようなもの。デフォルトでtoTensor()的な動作が入る
+    :type transform: Optional[Callable]
+    :return: MNIST + KMNISTのデータセット(train, test)
+    :rtype: tuple[Dataset, Dataset]
+    """
+    mnist_tf = transforms.ToTensor()
+    if transform is not None:
+        mnist_tf = transforms.Compose([mnist_tf, transform])
+    mnist_train = datasets.MNIST("../data", train=True, download=True, transform=mnist_tf)
+    mnist_test = datasets.MNIST("../data", train=False, transform=mnist_tf)
+    kmnist_train = NotNumberLabelKMNIST(data_size=len(mnist_train) // 10, train=True, transform=transform)
+    kmnist_test = NotNumberLabelKMNIST(data_size=len(mnist_test) // 10, train=False, transform=transform)
+    train = torch.utils.data.ConcatDataset([mnist_train, kmnist_train])
+    test = torch.utils.data.ConcatDataset([mnist_test, kmnist_test])
+
+    return train, test
